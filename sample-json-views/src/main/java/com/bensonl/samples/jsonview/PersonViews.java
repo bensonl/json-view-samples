ba42e2801eccb3d5bba4383a28f22fc8170da6e9
Benson Liu <bensonliu@Bensons-MacBook-Pro.local>
2017-04-05 21:18:59 -0700

    fix bug - views should be public so they can be used by other classes

--- sample-json-views/src/main/java/com/bensonl/samples/jsonview/PersonViews.java
+++ sample-json-views/src/main/java/com/bensonl/samples/jsonview/PersonViews.java
@@ -5,15 +5,15 @@ package com.bensonl.samples.jsonview;
  */
 public class PersonViews {
 
-    static class Default {}
+    public static class Default {}
 
-    interface Parents {}
+    public interface Parents {}
 
-    interface Siblings {}
+    public interface Siblings {}
 
-    static class WithParents extends Default implements Parents {}
+    public static class WithParents extends Default implements Parents {}
 
-    static class WithSiblings extends Default implements Siblings {}
+    public static class WithSiblings extends Default implements Siblings {}
 
     // TODO: Exercise - implement a view for WithChildren
     // TODO: Exercise - implement a WithFamily view that returns the person's information, his parents, siblings, and children
